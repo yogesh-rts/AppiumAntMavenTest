93bdab38ab7bb92f6babaa22c4e69bfbdb988eb1
YOGESH KUMAR <yogeshkumar@YOGESHs-MacBook-Pro.local>
2022-12-20 11:27:45 -0500

    Add Android skins

---
+++ src/test/resources/setup/emulator32_arm64.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+DEVICE_NAME="EMULATOR32"
+SYSTEM_IMAGE_PATH="system-images;android-32;google_apis;arm64-v8a"
+
+$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager --install ${SYSTEM_IMAGE_PATH}
+
+echo no | $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd -n ${DEVICE_NAME} -k ${SYSTEM_IMAGE_PATH} --force -d pixel_3a_xl
+$ANDROID_HOME/tools/emulator -avd ${DEVICE_NAME}
